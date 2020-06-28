2a02a313870e637a36a4d088e4491dd12ae7a7f1
marshmallowlands <2145136703@qq.com>
2020-06-28 17:44:12 +0800

    更新脚本

--- DLSHS-Tools-for-Windows.cmd
+++ DLSHS-Tools-for-Windows.cmd
@@ -8,38 +8,37 @@ cls
 echo 准备工作
 md src
 cd src
-..\bin\rm.exe -r *
 
 echo 下载文件
-..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/SubsetOTF/SourceHanSansCN.zip -O SourceHanSansCN.zip
-..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/SubsetOTF/SourceHanSansTW.zip -O SourceHanSansTW.zip
-..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/SubsetOTF/SourceHanSansHK.zip -O SourceHanSansHK.zip
-..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/SubsetOTF/SourceHanSansJP.zip -O SourceHanSansJP.zip
-..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/SubsetOTF/SourceHanSansKR.zip -O SourceHanSansKR.zip
+..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansSC.zip -O SourceHanSansSC.zip
+..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansTC.zip -O SourceHanSansTC.zip
+..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansHC.zip -O SourceHanSansHC.zip
+..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansJ.zip -O SourceHanSansJ.zip
+..\bin\wget.exe https://mirrors.bfsu.edu.cn/adobe-fonts/source-han-sans/OTF/SourceHanSansK.zip -O SourceHanSansK.zip
 
 echo 解压文件
-..\bin\unzip.exe SourceHanSansCN.zip
-..\bin\unzip.exe SourceHanSansTW.zip
-..\bin\unzip.exe SourceHanSansHK.zip
-..\bin\unzip.exe SourceHanSansJP.zip
-..\bin\unzip.exe SourceHanSansKR.zip
+..\bin\unzip.exe SourceHanSansSC.zip
+..\bin\unzip.exe SourceHanSansTC.zip
+..\bin\unzip.exe SourceHanSansHC.zip
+..\bin\unzip.exe SourceHanSansJ.zip
+..\bin\unzip.exe SourceHanSansK.zip
 
 echo 开始合并
 echo Noto Sans CJK Thin Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-ExtraLight.otf .\SourceHanSansKR\SourceHanSansKR-ExtraLight.otf .\SourceHanSansCN\SourceHanSansCN-ExtraLight.otf .\SourceHanSansTW\SourceHanSansTW-ExtraLight.otf .\SourceHanSansHK\SourceHanSansHK-ExtraLight.otf -o ..\output\NotoSansCJK-Thin.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-ExtraLight.otf .\SourceHanSansK\SourceHanSansK-ExtraLight.otf .\SourceHanSansSC\SourceHanSansSC-ExtraLight.otf .\SourceHanSansTC\SourceHanSansTC-ExtraLight.otf .\SourceHanSansHC\SourceHanSansHC-ExtraLight.otf -o ..\output\NotoSansCJK-Thin.ttc
 echo Noto Sans CJK Light Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-Light.otf .\SourceHanSansKR\SourceHanSansKR-Light.otf .\SourceHanSansCN\SourceHanSansCN-Light.otf .\SourceHanSansTW\SourceHanSansTW-Light.otf .\SourceHanSansHK\SourceHanSansHK-Light.otf -o ..\output\NotoSansCJK-Light.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-Light.otf .\SourceHanSansK\SourceHanSansK-Light.otf .\SourceHanSansSC\SourceHanSansSC-Light.otf .\SourceHanSansTC\SourceHanSansTC-Light.otf .\SourceHanSansHC\SourceHanSansHC-Light.otf -o ..\output\NotoSansCJK-Light.ttc
 echo Noto Sans CJK Regular Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-Regular.otf .\SourceHanSansKR\SourceHanSansKR-Regular.otf .\SourceHanSansCN\SourceHanSansCN-Regular.otf .\SourceHanSansTW\SourceHanSansTW-Regular.otf .\SourceHanSansHK\SourceHanSansHK-Regular.otf -o ..\output\NotoSansCJK-Regular.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-Regular.otf .\SourceHanSansK\SourceHanSansK-Regular.otf .\SourceHanSansSC\SourceHanSansSC-Regular.otf .\SourceHanSansTC\SourceHanSansTC-Regular.otf .\SourceHanSansHC\SourceHanSansHC-Regular.otf -o ..\output\NotoSansCJK-Regular.ttc
 echo Noto Sans CJK Medium Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-Medium.otf .\SourceHanSansKR\SourceHanSansKR-Medium.otf .\SourceHanSansCN\SourceHanSansCN-Medium.otf .\SourceHanSansTW\SourceHanSansTW-Medium.otf .\SourceHanSansHK\SourceHanSansHK-Medium.otf -o ..\output\NotoSansCJK-Medium.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-Medium.otf .\SourceHanSansK\SourceHanSansK-Medium.otf .\SourceHanSansSC\SourceHanSansSC-Medium.otf .\SourceHanSansTC\SourceHanSansTC-Medium.otf .\SourceHanSansHC\SourceHanSansHC-Medium.otf -o ..\output\NotoSansCJK-Medium.ttc
 echo Noto Sans CJK Bold Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-Bold.otf .\SourceHanSansKR\SourceHanSansKR-Bold.otf .\SourceHanSansCN\SourceHanSansCN-Bold.otf .\SourceHanSansTW\SourceHanSansTW-Bold.otf .\SourceHanSansHK\SourceHanSansHK-Bold.otf -o ..\output\NotoSansCJK-Bold.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-Bold.otf .\SourceHanSansK\SourceHanSansK-Bold.otf .\SourceHanSansSC\SourceHanSansSC-Bold.otf .\SourceHanSansTC\SourceHanSansTC-Bold.otf .\SourceHanSansHC\SourceHanSansHC-Bold.otf -o ..\output\NotoSansCJK-Bold.ttc
 echo Noto Sans CJK Black Fonts
-..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJP\SourceHanSansJP-Heavy.otf .\SourceHanSansKR\SourceHanSansKR-Heavy.otf .\SourceHanSansCN\SourceHanSansCN-Heavy.otf .\SourceHanSansTW\SourceHanSansTW-Heavy.otf .\SourceHanSansHK\SourceHanSansHK-Heavy.otf -o ..\output\NotoSansCJK-Black.ttc
+..\bin\python\python.exe ..\bin\afdko\otf2otc.py .\SourceHanSansJ\SourceHanSans-Heavy.otf .\SourceHanSansK\SourceHanSansK-Heavy.otf .\SourceHanSansSC\SourceHanSansSC-Heavy.otf .\SourceHanSansTC\SourceHanSansTC-Heavy.otf .\SourceHanSansHC\SourceHanSansHC-Heavy.otf -o ..\output\NotoSansCJK-Black.ttc
 
 echo 清理临时文件
 ..\bin\rm.exe -r *
 
-echo 完事了，祝你开心每一天2333
+echo 完事了，小花花送你~
 pause
